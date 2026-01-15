f233cb2ef6ee4f0ee2484797682dc09b38506455
Aarsh <aarshmuley26@gmail.com>
2026-01-15 21:06:26 +0530

    fix: update sw cache version to force client update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'adapt-maze-v1';
+const CACHE_NAME = 'adapt-maze-v2';
 const ASSETS = [
     './',
     './index.html',
@@ -8,8 +8,20 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
     );
 });
 
