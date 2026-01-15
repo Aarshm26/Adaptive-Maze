4d40bc16eb529b3ba6dcd22a48055c8980db3d8b
Aarsh <aarshmuley26@gmail.com>
2026-01-15 21:17:28 +0530

    deploy: move production assets to /docs for GH Pages compatibility

---
+++ docs/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'adapt-maze-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './index.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
